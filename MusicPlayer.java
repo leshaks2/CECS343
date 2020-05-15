30e8ee15f0b3d5c25e3b39d092b18f24e235623c
Matthew Ortiz <58668605+matthewo22@users.noreply.github.com>
2020-05-14 17:12:31 -0700

    Add files via upload

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package musicplayer;
 
 import javafx.application.Application;
@@ -20,13 +25,23 @@ public class MusicPlayer extends Application {
         
         stage.setTitle("Music Player");
         stage.setScene(scene);
-        stage.show();
+        stage.show();  
     }
+    
  
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    launch(args);
+        LoginForm x = new LoginForm();
+        x.setVisible(true);
+        boolean test = false;
+        while (test == false){
+            test = x.getTest();
+            System.out.println();
+        }
+        if (test==true){
+            launch(args);
+        }
     }
 }
